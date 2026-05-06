87213ce87ed71a9b6b63caca98d432cd070077ef
Vianelo04 <pepeposacco@gmail.com>
2026-05-06 12:21:55 -0300

    E3 - Função que recebe e retorna várias Strings

---
+++ E3.js
@@ -0,0 +1,8 @@
+function lista(...string) {
+    console.log(string.join(", "));
+}
+
+lista("computador");
+lista("pessoa", "mouse");
+lista("pessoa", "mouse", "teclado");
+lista("pessoa", "mouse", "teclado", "a", "b", "c", "d", "f");
