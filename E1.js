2628a48cb4fee54ffa116011a600157b720f8b91
Vianelo04 <pepeposacco@gmail.com>
2026-05-06 10:24:23 -0300

    E1 - String de maior comprimento

---
+++ E1.js
@@ -0,0 +1,8 @@
+function maiorString(a, b) {
+  return a.length >= b.length ? a : b;
+}
+
+console.log(maiorString("aaa", "bbbb"));
+console.log(maiorString("aaaa", "bbb"));
+console.log(maiorString("aaaa", "bbbb"));
+
