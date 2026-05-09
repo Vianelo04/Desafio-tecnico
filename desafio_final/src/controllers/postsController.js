843d5792765a3e091bdb7f671696e0095a936553
Vianelo04 <pepeposacco@gmail.com>
2026-05-09 17:41:46 -0300

    feat: implementa endpoint GET /posts/:id

--- desafio_final/src/controllers/postsController.js
+++ desafio_final/src/controllers/postsController.js
@@ -9,4 +9,20 @@ const listarPosts = async (req, res) => {
     }
 };
 
-module.exports = { listarPosts };
+const obterPosts = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const post = await connection('posts').where({id}).first();
+
+        if (!post){
+            return res.status(404).json({erro: 'erro post não encontrado'});
+        }
+
+        return res.status(200).json(post);
+    } catch (error) {
+        return res.status(500).json({ erro: 'erro ao listar post'});
+    }
+};
+
+module.exports = { listarPosts, obterPosts };
--- desafio_final/src/routes/postsRoutes.js
+++ desafio_final/src/routes/postsRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const postsController = require('../controllers/postsController');
 
 router.get('/', postsController.listarPosts);
+router.get('/:id', postsController.obterPosts);
+
 
 module.exports = router;
